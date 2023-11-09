fe2db3e90cec04a6b8ba62cd9fc6cbfbcac0616d
CarlosC <Carlos.manuelct17@gmail.com>
2023-11-09 17:24:03 -0500

    verifiacion

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //permitir que se puedan ahcer peticines desde mi localhost:4200 en las corsoptions de nestjs?
   
-  app.enableCors({ origin: ['http://localhost:4200'] });
+  app.enableCors({ origin: 'http://localhost:4200' });
   await app.listen(3000);
 }
 bootstrap();
